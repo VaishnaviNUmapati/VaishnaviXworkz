2e43f6e29e3fe482a7b6e2ea8dc2beb492bb7dee
VaishnaviNUmapati <76847612+VaishnaviNUmapati@users.noreply.github.com>
2021-01-02 21:36:40 +0530

    Add files via upload

---
+++ Divison.java
@@ -0,0 +1,10 @@
+class Divison
+{
+  public static void main(String a[])
+  {
+	  int num1=100;
+	  int num2=5;
+	  int num3=num1/num2;
+  System.out.println(num3);
+  }
+  }
---
+++ Fact.java
@@ -0,0 +1,17 @@
+class Factorial
+{
+public static void main(String a[])
+{
+int num=8;
+fact(num);
+}
+static void fact(int num)
+{
+int f=1;
+for(int i=1;i<=num;i++)
+{
+f=f*i;
+System.out.println(f);
+}
+}
+}
