d7b82037275da494c001885611493b98dbb7590c
VaishnaviNUmapati <76847612+VaishnaviNUmapati@users.noreply.github.com>
2021-01-02 21:44:24 +0530

    Add files via upload

---
+++ Bank.java
@@ -0,0 +1,14 @@
+class Account
+{
+public static void main(String a[])
+{
+  String name= "Vaishnavi";
+  int balance=100000;
+  String address="Hubli,India";
+  long phoneNumber=924241872;
+   System.out.println(name);
+  System.out.println(balance);
+  System.out.println(address);
+  System.out.println(phoneNumber);
+  }
+  }
---
+++ City.java
@@ -0,0 +1,7 @@
+class City
+{
+  public static void main(String a[])
+  {
+  System.out.println(1+2);
+  }
+  }
